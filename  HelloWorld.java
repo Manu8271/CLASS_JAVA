c9977c4687fdd51290fc3720aa16b14962f73f2e
zen29062001 <110222664+zen29062001@users.noreply.github.com>
2023-06-04 20:38:23 +0530

    Add files via upload

---
+++  HelloWorld.java
@@ -0,0 +1,18 @@
+// Name - Manu yadav
+// REG No - 20BCE10290
+// PRINT HELLO WORLD IN JAVA
+
+
+
+class HelloWorld
+{
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World");
+    }
+}
+
+
+
---
+++ import java.util.java
@@ -0,0 +1,22 @@
+//Name - Manu Yadav
+// REG No - 20BCE10290
+
+// Java program to accept input from keyboard using scanner class
+
+
+import java.util.*;  
+class UserInputDemo   
+{  
+public static void main(String[] args)  
+{  
+Scanner sc= new Scanner(System.in);    //System.in is a standard input stream  
+System.out.print("Enter first number- ");  
+int a= sc.nextInt();  
+System.out.print("Enter second number- ");  
+int b= sc.nextInt();  
+System.out.print("Enter third number- ");  
+int c= sc.nextInt();  
+int d=a+b+c;  
+System.out.println("Total= " +d);  
+}  
+}  
