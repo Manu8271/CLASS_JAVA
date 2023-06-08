75a423f5bb88580e311581d6db9ce15ca77e5726
zen29062001 <110222664+zen29062001@users.noreply.github.com>
2023-06-08 21:59:17 +0530

    Update import java.util.java

--- import java.util.java
+++ import java.util.java
@@ -1,14 +1,10 @@
 
-
-// Java program to accept input from keyboard using scanner class
-
-
 import java.util.*;  
 class UserInputDemo   
 {  
 public static void main(String[] args)  
 {  
-Scanner sc= new Scanner(System.in);    //System.in is a standard input stream  
+Scanner sc= new Scanner(System.in);    
 System.out.print("Enter first number- ");  
 int a= sc.nextInt();  
 System.out.print("Enter second number- ");  
